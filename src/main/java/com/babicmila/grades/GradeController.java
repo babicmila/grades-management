e843b60b69b59d16a3c1d1e0f4b5edde1012a3bd
Mila Babic <milababic5@gmail.com>
2023-06-25 00:00:40 +0200

    updated grade controller and grades template

--- src/main/java/com/babicmila/grades/GradeController.java
+++ src/main/java/com/babicmila/grades/GradeController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.babicmila.Grade;
+
 @Controller
 public class GradeController {
-    
 
     @GetMapping("/grades")
-    public String getGrades(Model model){
+    public String getGrades(Model model) {
+
+        Grade grade = new Grade("Mila", "OOP", "A+");
+        model.addAttribute("grade", grade);
+
         return "grades";
     }
 
