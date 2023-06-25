ff8ad670eecda17e6dbb97375ee7120baa3d1e01
Mila Babic <milababic5@gmail.com>
2023-06-25 03:35:53 +0200

    added thymeleaf loop

--- src/main/java/com/babicmila/grades/GradeController.java
+++ src/main/java/com/babicmila/grades/GradeController.java
@@ -1,5 +1,9 @@
 package com.babicmila.grades;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +13,16 @@ import com.babicmila.Grade;
 @Controller
 public class GradeController {
 
+    List<Grade> gradeslist = Arrays.asList(
+        new Grade("Mila", "OOP", "A+"),
+        new Grade("Ana", "Math", "B+"),
+        new Grade("Milos", "Art history", "A-")
+    );
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
 
-        Grade grade = new Grade("Mila", "OOP", "A+");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", gradeslist);
 
         return "grades";
     }
